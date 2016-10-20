dcaa7b65b4e420cb1664f4c7d63f6fa46d00960b
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-10-20 07:40:02 -0400

    Splash Screen Done

--- COMP1004-F2016-Mid-Term-200277598/Program.cs
+++ COMP1004-F2016-Mid-Term-200277598/Program.cs
@@ -19,7 +19,7 @@ namespace COMP1004_F2016_Mid_Term_200277598
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AbilityGeneratorForm());
+            Application.Run(new SplashForm());
         }
     }
 }
---
+++ COMP1004-F2016-Mid-Term-200277598/SplashForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_F2016_Mid_Term_200277598
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            SplashFormTimer.Enabled = false;
+
+            GenerateNameForm generateNameForm = new GenerateNameForm();
+            generateNameForm.Show();
+            this.Hide();
+
+        }
+    }
+}
